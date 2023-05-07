777534f48237c4f97c8550b68c47eaaa454e5a07
mei-ldm <meiliuxy888@gmail.com>
2023-05-07 00:13:35 -0500

    my first commit

---
+++ src/main/java/com/laioffer/jupiter/entity/response/ItemType.java
@@ -0,0 +1,12 @@
+package com.laioffer.jupiter.entity.response;
+
+public enum ItemType {
+//    public static ItemType STREAM;
+//    public static ItemType CLIP;
+//    public static ItemType VIDEO;
+//
+//    public static ItemType[] values() {
+//    }
+
+    STREAM, VIDEO, CLIP
+}
